7a3447f8526934b11940fe59745db13e2075ac7f
TheSETJ <ehsantaheri25@yahoo.com>
2016-02-27 10:41:48 +0000

    Add functionality: loading data

--- public/js/controllers/todo-controller.js
+++ public/js/controllers/todo-controller.js
@@ -6,4 +6,19 @@ angular.module('todoGamify').controller('TodoController', function() {
   todoCtrl.input = {
     brief: null
   };
+  
+  // load lists from local storage
+  todoCtrl.load = function load() {
+    var content = null;
+    
+    // convert and load data into active todo list, if there is no data then load in an empty array 
+    content = window.localStorage.getItem("todo-gamify-atl") || [];
+    todoCtrl.activeTodoList = JSON.parse(content);
+    
+    // convert and load data into finished todo list, if there is no data then load in an empty array 
+    content = window.localStorage.getItem("todo-gamify-ftl") || [];
+    todoCtrl.finishedTodoList = JSON.parse(content);
+  };
+
+  todoCtrl.load();  
 });
